d9331ba8da9b0ca26051ac842d32005e4b5d8240
Marci310 <sipos.marci31@gmail.com>
2025-02-27 13:13:17 +0100

    added tailwndcss

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var express = require('express');
-var app = express;
+const express = require('express');
+const app = express();
 
 app.use(express.static('static'));
-
-var server = app.listen(3000, function(){
-    
-})
+app.listen(3000, function () {
+    console.log('Server is running on http://localhost:3000');
+});
