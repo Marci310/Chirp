1652a9fbd5e05a55730e772f08a86279c6edfc32
Marci310 <sipos.marci31@gmail.com>
2025-03-02 23:23:19 +0100

    sidebar-phase1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 app.listen(3000, function () {
-    console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
